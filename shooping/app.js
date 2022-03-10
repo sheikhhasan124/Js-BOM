0405967e25ce775f3c1bcfbee77f68ebf34d6b09
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-10 11:35:43 +0600

    set loca stor

--- shooping/app.js
+++ shooping/app.js
@@ -1,11 +1,30 @@
-
+let productContainer = document.getElementById('product-container')
 
 const addItem=()=>{
     let productName = document.getElementById('product-name');
     let productValue = productName.value;
-    console.log(productValue)
+    if(!productValue){
+        return;
+    }
+    // console.log(productValue)
+    // display product
+    displayProduct(productValue)
+    // add localstora
+    addToLocaStorage(productValue)
     productName.value = '';
 }
-const displayProduct = ()=>{
+const displayProduct = (product)=>{
+    
     let li = document.createElement('li');
+    li.innerText = product;
+    productContainer.appendChild(li)
+}
+const addToLocaStorage=(product)=>{
+   localStorage.setItem('item',product)
+}
+
+
+const placeOrder = ()=>{
+   productContainer.textContent = '';
+
 }
